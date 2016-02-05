57d2121bdde8a32f0d17db91a39e5031450a47b5
Benoit Sagols <benoit.sagols@gmail.com>
2016-02-05 22:32:06 +0700

    Added support for Resource Controllers and Nested Resource Controllers

--- config/autoroute.php
+++ config/autoroute.php
@@ -2,7 +2,7 @@
 
 return [
     'routes' => [
-        // [null, 'index.home']
+        // ['index.index'] // Ex: home..
     ],
 
     'constraints' => [
@@ -10,7 +10,19 @@ return [
     ],
 
     'options' => [
-        // 'separator' => '.',
-        // 'route_name' => '{ctrl}.{action}',
+        // All defaults
+        // 'ignore_index' => true,
+        // 'ctrl_separator' => '.',
+        // 'route_separator' => '.',
+        // 'filters' => ['snake', 'slug'],
+        // 'resource_names' => [
+        //     'index'   => true,
+        //     'create'  => true,
+        //     'store'   => true,
+        //     'show'    => true,
+        //     'edit'    => true,
+        //     'update'  => true,
+        //     'destroy' => true
+        // ]
     ],
 ];
--- src/Autoroute.php
+++ src/Autoroute.php
@@ -9,53 +9,112 @@ class Autoroute {
     const IS_VERB = '/^(get|post|put|delete|any)$/i';
 
     protected $router;
-    protected $routes;
+    protected $routes = [];
     protected $requirements;
     protected $options;
 
     protected $defaults = [
         'ignore_index' => true,
-        'separator' => '.',
-        'route_name' => '{ctrl}.{action}',
-        'filters' => ['snake', 'slug']
+        'ctrl_separator' => '.',
+        'route_separator' => '.',
+        'filters' => ['snake', 'slug'],
+        'resource_names' => [
+            'index'   => true,
+            'create'  => true,
+            'store'   => true,
+            'show'    => true,
+            'edit'    => true,
+            'update'  => true,
+            'destroy' => true
+        ]
     ];
 
     public function __construct(Router $router, array $routes, array $requirements, $options = [])
     {
         $this->router = $router;
-        $this->routes = $routes;
         $this->requirements = $requirements;
         $this->options = array_merge($this->defaults, $options);
         $this->filters = array_filter($this->options['filters'], function($filter) {
             return in_array($filter, ['slug', 'snake', 'camel']);
         });
+
+        $this->routes['app'] = [];
+
+        foreach ($routes as $key => $route) {
+            if (is_numeric($key)) {
+                array_push($this->routes['app'], $route);
+            } else {
+                $this->routes[$key] = $route; // Here `$route` is an array of routes.
+            }
+        }
     }
 
-    public function make()
+    public function make($namespace = 'app')
+    {
+        if (!array_key_exists($namespace, $this->routes)) {
+            throw new \Exception('Namespace "'.$namespace.'" doesn\'t exist.');
+        }
+        return $this->makeRoutes($this->routes[$namespace], $namespace);
+    }
+
+    public function makeRoutes(array $definitions, $namespace)
     {
         $routes = [];
-        foreach ($this->routes as $route) {
-            $ctrl = key($route);
+        foreach ($definitions as $route) {
+            list(
+                $ctrl,
+                $pathname,
+                $verb,
+                $name,
+                $options) = call_user_func([$this, 'normalizeRoute'], $route);
 
-            if (is_string($ctrl)) {
-                $pathname = current($route);
-            } else {
-                $ctrl = current($route);
-                $pathname = null;
-            }
-            array_shift($route);
-            array_unshift($route, $ctrl, $pathname);
-
-            $routes[] = call_user_func_array([$this, 'makeRoute'], $route);
+            $routes[] = $this->doMakeRoute($namespace, $ctrl, $pathname, $verb, $name, $options);
         }
         return $routes;
     }
 
-    public function makeRoute($ctrl, $pathname, $verb = null, $name = null)
+
+    public function makeRoute($ctrl, $pathname, $verb = null, $name = null, $namespace = 'app')
     {
-        if ($verb) {
+        list(
+            $ctrl,
+            $pathname,
+            $verb,
+            $name,
+            $options) = $this->normalizeRoute([$ctrl, $pathname, $verb, $name]);
+
+        return $this->doMakeRoute($namespace, $ctrl, $pathname, $verb, $name, $options);
+    }
+
+    protected function normalizeRoute(array $route)
+    {
+        $options = [];
+
+        $ctrl = key($route);
+
+        if (is_string($ctrl)) {
+            $pathname = current($route);
+        } else {
+            $ctrl = current($route);
+            $pathname = null;
+        }
+
+        // Reset index
+        $route = array_values($route);
+
+        $name = null;
+        $verb = null;
+        if (count($route) > 1) {
+            $verb = $route[1];
             if (is_array($verb)) {
-                $match = $verb;
+                // Match
+                $verb = array_map(function($verbe) {
+                    return strtolower($verbe);
+                }, $verb);
+            } else if ($verb === 'resource') {
+                if (count($route) > 2) {
+                    $options = $route[2];
+                }
             } else if (preg_match(Autoroute::IS_VERB, $verb) === 1) {
                 // Normalize $verb
                 $verb = strtolower($verb);
@@ -70,21 +129,30 @@ class Autoroute {
             $verb = 'get';
         }
 
-        list($ctrl, $action, $namespace) = $this->parseController($ctrl);
+        return [$ctrl, $pathname, $verb, $name, $options];
+    }
+
+    protected function doMakeRoute($namespace, $ctrl, $pathname, $verb, $name, $options)
+    {
+        if ($verb === 'resource') {
+            return $this->doMakeResourceRoute($ctrl, $namespace, $options);
+        }
+
+        list($namespace, $ctrl, $action) = $this->parseCtrl($ctrl, $namespace);
 
         $controller = $this->getController($ctrl, $action, $namespace);
 
         if (!$pathname) {
-            $pathname = $this->getPath($ctrl, $action);
+            $pathname = $this->getPathname($ctrl, $action, $namespace);
         }
         if (!$name) {
-            $name = $this->getRouteName($ctrl, $action);
+            $name = $this->getRouteName($ctrl, $action, $namespace);
         }
 
         // dd($ctrl, $pathname, $verb, $name);
 
-        if (isset($match)) {
-            $route = call_user_func([$this->router, 'match'], $match, $pathname, $controller);
+        if (is_array($verb)) {
+            $route = call_user_func([$this->router, 'match'], $verb, $pathname, $controller);
         } else {
             $route = call_user_func([$this->router, $verb], $pathname, $controller);
         }
@@ -110,58 +178,110 @@ class Autoroute {
         return $route;
     }
 
-    protected function parseController($ctrl)
+    protected function doMakeResourceRoute($resource, $namespace, $options)
     {
-        $crumbs = explode($this->options['separator'], $ctrl);
-        $action = array_pop($crumbs);
-        $ctrl = array_pop($crumbs);
+        $controller = $this->getResourceController($resource, $namespace);
 
-        $namespace = null;
-        if (count($crumbs)) {
-            $namespace = implode('\\', array_map(function($crumb) {
-                return ucfirst($crumb);
-            }, $crumbs));
+        if (!array_key_exists('names', $options)) {
+            $options['names'] = $this->getResourceNames($resource, $namespace);
         }
 
-        return [$ctrl, $action, $namespace];
+        return $this->router->resource($resource, $controller, $options);
     }
 
-    protected function getPath($ctrl, $action)
+    public function getResourceNames($resource, $namespace)
+    {
+        $names = [];
+        foreach ($this->options['resource_names'] as $key => $name) {
+            if ($name === true) {
+                $name = $key;
+            }
+            $names[$key] = implode('.', [$namespace, $resource, $name]);
+        }
+        return $names;
+    }
+
+    protected function parseCtrl($ctrl, $namespace)
+    {
+        $segments = explode($this->options['ctrl_separator'], $ctrl);
+        $action = array_pop($segments);
+        $ctrl = array_pop($segments);
+
+        if ($namespace === 'app') {
+            $namespace = $segments;
+        } else {
+            $namespace = array_merge([$namespace], $segments);
+        }
+
+        return [$namespace, $ctrl, $action];
+    }
+
+    protected function getController($ctrl, $action, array $namespace)
+    {
+        $ctrl = ucfirst($ctrl);
+
+        if (!count($namespace)) {
+            return "{$ctrl}Controller@{$action}";
+        }
+
+        $namespace = array_map(function($segment) {
+            return ucfirst($segment);
+        }, $namespace);
+
+        $namespace = implode('\\', $namespace);
+
+        return "{$namespace}\\{$ctrl}Controller@{$action}";
+    }
+
+    protected function getResourceController($resource, $namespace)
+    {
+        $resource = explode('.', $resource);
+
+        $controller = array_map(function($segment) {
+            return ucfirst($segment);
+        }, $resource);
+
+        if ($namespace !== 'app') {
+            array_unshift($controller, ucfirst($namespace));
+        }
+
+        return implode('\\', $controller).'Controller';
+    }
+
+    protected function getPathname($ctrl, $action, array $namespace)
     {
         $pathname = [$ctrl, $action];
+
+        if (count($namespace)) {
+            $pathname = array_merge($namespace, $pathname);
+        }
+
         if ($this->options['ignore_index']) {
-            if ($ctrl === 'index') {
-                array_shift($pathname);
-            }
-            if ($action === 'index') {
-                array_shift($pathname);
-            }
+            $pathname = array_filter($pathname, function($crumb) {
+                return $crumb !== 'index';
+            });
         }
         return implode('/', $pathname);
     }
 
-    protected function getRouteName($ctrl, $action)
+    protected function getRouteName($ctrl, $action, array $namespace)
     {
         $ctrl   = $this->transformName($ctrl);
         $action = $this->transformName($action);
+        $sep    = $this->options['route_separator'];
 
-        $route = $this->options['route_name'];
-        $route = str_replace('{ctrl}', $ctrl, $route);
-        $route = str_replace('{action}', $action, $route);
+        if (!count($namespace)) {
+            return "{$ctrl}{$sep}{$action}";
+        }
 
-        return $route;
-    }
+        $autoroute = $this;
+        $pieces = array_map(function($segment) use ($autoroute) {
+            return $autoroute->transformName($segment);
+        }, $namespace);
 
-    protected function getController($ctrl, $action, $namespace = null)
-    {
-        $controller = $namespace ? [$namespace, '\\'] : [];
-        $controller = array_merge($controller, [
-            ucfirst($ctrl),
-            'Controller@',
-            $action
-        ]);
+        array_push($pieces, $route);
 
-        return implode('', $controller);
+        return implode($sep, $pieces);
     }
 
     protected function transformName($name)
